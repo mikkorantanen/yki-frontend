8555e5360dfc06d47a93972c310f3bb55e350a5d
Matias Kantele <matias.kantele@siili.com>
2018-10-25 16:17:33 +0300

    add page for 404 not found

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import { Route, Switch } from 'react-router-dom';
 import ophStyles from './assets/css/oph-styles.css';
 import Layout from './hoc/Layout/Layout';
 import OrganizerRegistry from './containers/OrganizerRegistry/OrganizerRegistry';
-import NewOrganizer from './containers/Organizers/AddOrganizer/AddOrganizer';
+import AddOrganizer from './containers/Organizers/AddOrganizer/AddOrganizer';
+import NotFound from './components/NotFound/NotFound';
 
 class App extends Component {
   render() {
@@ -21,6 +22,7 @@ class App extends Component {
             <Route path="/jarjestajarekisteri/uusi" component={AddOrganizer} />
             <Route path="/tutkintatilaisuudet" component={OrganizerRegistry} />
             <Route path="/jarjestajarekisteri" component={OrganizerRegistry} />
+            <Route component={NotFound} />
           </Switch>
         </Layout>
       </div>
