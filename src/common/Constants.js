94b33b8616ba98c2565b32af60dfd520d85ded2d
Matias Kantele <matias.kantele@siili.com>
2018-10-25 16:20:08 +0300

    add multi-select component for choosing languages

--- src/common/Constants.js
+++ src/common/Constants.js
@@ -1,1 +1,48 @@
 export const DATE_FORMAT = 'D.M.YYYY';
+export const LANGUAGES = [
+  {
+    code: 'fi',
+    name: 'Suomi',
+    levels: ['PERUS', 'KESKI', 'YLIN'],
+  },
+  {
+    code: 'sv',
+    name: 'Ruotsi',
+    levels: ['PERUS', 'KESKI', 'YLIN'],
+  },
+  {
+    code: 'en',
+    name: 'Englanti',
+    levels: ['PERUS', 'KESKI', 'YLIN'],
+  },
+  {
+    code: 'es',
+    name: 'Espanja',
+    levels: ['PERUS', 'KESKI', 'YLIN'],
+  },
+  {
+    code: 'it',
+    name: 'Italia',
+    levels: ['PERUS', 'KESKI'],
+  },
+  {
+    code: 'fr',
+    name: 'Ranska',
+    levels: ['PERUS', 'KESKI', 'YLIN'],
+  },
+  {
+    code: 'se',
+    name: 'Pohjoissaame',
+    levels: ['PERUS', 'KESKI', 'YLIN'],
+  },
+  {
+    code: 'de',
+    name: 'Saksa',
+    levels: ['PERUS', 'KESKI', 'YLIN'],
+  },
+  {
+    code: 'ru',
+    name: 'Venäjä',
+    levels: ['PERUS', 'KESKI', 'YLIN'],
+  },
+];
