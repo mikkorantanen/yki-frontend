59a019b55111dbcb825f682ae7fd804e809f23fd
Tom Bertell <tom.bertell@siili.com>
2018-10-30 07:39:18 +0200

    spring boot project structure

---
+++ src/main/java/fi/oph/yki/model/LogEntry.java
@@ -0,0 +1,14 @@
+package fi.oph.yki.model;
+
+public class LogEntry {
+
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
