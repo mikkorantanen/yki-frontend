71b8274c9d28900034fa726492f6bf1fc989b0f2
Matias Kantele <matias.kantele@siili.com>
2018-10-30 15:05:01 +0200

    add placeholder container and route for exam sessions

---
+++ src/containers/ExamSessions/ExamSessions.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+import classes from './ExamSessions.module.css';
+
+class ExamSessions extends Component {
+  render() {
+    return <div className={classes.ExamSessions}>ExamSessions</div>;
+  }
+}
+
+export default ExamSessions;
