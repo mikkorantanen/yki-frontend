6b8b882dfd6a3733dfa71abcbbbfa0b55d569b6b
Tom Bertell <tom.bertell@siili.com>
2018-11-06 08:21:31 +0200

    removed console log

--- src/main/js/cypress/integration/auth_spec.js
+++ src/main/js/cypress/integration/auth_spec.js
@@ -1,8 +1,6 @@
 describe('Authorization', () => {
   if (Cypress.config('baseUrl').includes('https')) {
     it('init session endpoint should not be accessible', () => {
-      console.log('XXXXXXXXXXXXX');
-      console.log(Cypress.config('baseUrl'));
       cy.request({
         url: '/yki/auth/initsession',
         failOnStatusCode: false,
