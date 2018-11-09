f759ad8746835eb1e0221bab0c6bf1cbe78ebd53
Tom Bertell <tom.bertell@siili.com>
2018-11-09 10:27:48 +0200

    fixed syntax error

--- scripts/ci-build.sh
+++ scripts/ci-build.sh
@@ -5,7 +5,7 @@ if [ "${TRAVIS_EVENT_TYPE}" = "cron" ]; then
   CYPRESS_baseUrl=https://virkailija.untuvaopintopolku.fi npm run cypress:run --prefix src/main/js/
 fi
 if [ "${TRAVIS_EVENT_TYPE}" != "cron" ]; then
-  npm test --prefix src/main/js/"
+  npm test --prefix src/main/js/
   ./mvnw clean install
   mv target/yki-frontend-*.jar $DOCKER_BUILD_DIR/artifact/${ARTIFACT_NAME}.jar
   cp -vr src/main/resources/* $DOCKER_BUILD_DIR/config/
