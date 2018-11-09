312ce84056d963ff7b99283656ba0b5f65cb2ccb
Tom Bertell <tom.bertell@siili.com>
2018-11-09 10:19:03 +0200

    execute e2e tests when ci job is triggered by cron

---
+++ scripts/ci-build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -ev
+npm i --prefix src/main/js/
+if [ "${TRAVIS_EVENT_TYPE}" = "cron" ]; then
+  CYPRESS_baseUrl=https://virkailija.untuvaopintopolku.fi npm run cypress:run --prefix src/main/js/
+fi
+if [ "${TRAVIS_EVENT_TYPE}" != "cron" ]; then
+  npm test --prefix src/main/js/"
+  ./mvnw clean install
+  mv target/yki-frontend-*.jar $DOCKER_BUILD_DIR/artifact/${ARTIFACT_NAME}.jar
+  cp -vr src/main/resources/* $DOCKER_BUILD_DIR/config/
+  export BASE_IMAGE="baseimage-fatjar:latest-master"
+  ./ci-tools/common/pull-image.sh
+  ./ci-tools/build/build-fatjar.sh $ARTIFACT_NAME
+fi
