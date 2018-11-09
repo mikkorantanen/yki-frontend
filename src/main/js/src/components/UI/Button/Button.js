51eeb9e14444064b3f126bbec4fdb8c9eca1f140
Matias Kantele <matias.kantele@siili.com>
2018-11-09 21:48:26 +0200

    remove unnecessary old oph-styles css file

--- src/main/js/src/components/UI/Button/Button.js
+++ src/main/js/src/components/UI/Button/Button.js
@@ -2,18 +2,12 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import classes from './Button.module.css';
-import ophStyles from '../../../assets/css/oph-styles.css';
 
 const button = props => (
   <button
     type={props.type}
     disabled={props.disabled}
-    className={[
-      ophStyles['oph-button'],
-      ophStyles['oph-button-primary'],
-      classes.Button,
-      classes[props.btnType],
-    ].join(' ')}
+    className={[classes.Button, classes[props.btnType]].join(' ')}
     onClick={props.clicked}
   >
     {props.children}
