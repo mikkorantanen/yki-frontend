61688ebc1debc2b698f02e13c009a3d2cbac9197
matias.kantele <matias.kantele@siili.com>
2018-11-22 10:44:25 +0200

    stop console warnings when testing app from using basename on router

--- src/main/js/src/App.test.js
+++ src/main/js/src/App.test.js
@@ -5,6 +5,7 @@ import { StaticRouter } from 'react-router-dom';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
+  window.history.pushState({}, 'yki', '/yki/'); // stop console warnings from using basename
 
   const app = (
     <StaticRouter context={{}}>
