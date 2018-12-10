edddae69c9f0ffed480ebcb629bf3b2146e62cf0
Matias Kantele <matias.kantele@siili.com>
2018-12-10 18:01:42 +0200

    add custom checkbox component for language selection

--- src/main/js/src/components/LanguageCheckboxes/LanguageCheckboxes.js
+++ src/main/js/src/components/LanguageCheckboxes/LanguageCheckboxes.js
@@ -1,71 +1,68 @@
-import React, { PureComponent } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 
 import classes from './LanguageCheckboxes.module.css';
 import { LANGUAGES } from '../../common/Constants';
 import Checkbox from '../UI/Checkbox/Checkbox';
 
-class LanguageCheckboxes extends PureComponent {
-  state = {
-    languages: [],
+const languageCheckboxes = props => {
+  // console.log(props.languages);
+  const toggleChecked = (code, level) => {
+    let updatedLanguages = props.languages.some(
+      l => l.language_code === code && l.level_code === level,
+    )
+      ? props.languages.filter(
+          l => l.language_code !== code || l.level_code !== level,
+        )
+      : [...props.languages, { language_code: code, level_code: level }];
+
+    props.onChange(updatedLanguages);
   };
 
-  toggleChecked(language) {
-    const languages = [...this.state.languages];
-    let updatedLanguages = [];
-    if (
-      languages.some(
-        l =>
-          l.language_code === language.language_code &&
-          l.level_code === language.level_code,
-      )
-    ) {
-      updatedLanguages = languages.filter(
-        l =>
-          l.language_code !== language.code &&
-          l.level_code !== language.level_code,
-      );
-    } else {
-      updatedLanguages = [...languages, language];
-    }
-    this.setState({ languages: updatedLanguages });
-    this.props.onUpdate(updatedLanguages);
-  }
-
-  render() {
-    const levels = ['Perustaso', 'Keskitaso', 'Ylin taso'];
-    const languageLabels = (
-      <div className={classes.LanguageLabels}>
-        <span className={classes.LanguageLabel} />
-        {levels.map(level => (
-          <span key={level} className={classes.LanguageLabel}>
-            {level}
-          </span>
-        ))}
-      </div>
+  const isSelected = (languageCode, levelCode) =>
+    props.languages.some(
+      l => l.language_code === languageCode && l.level_code === levelCode,
     );
 
-    const checkboxGrid = LANGUAGES.map(l => (
-      <div key={l.name} className={classes.CheckboxGrid}>
-        <div className={classes.Language}>{l.name}</div>
-        {l.levels.map(ll => (
-          <Checkbox
-            key={ll}
-            className={classes.Checkbox}
-            clicked={() =>
-              this.toggleChecked({ language_code: l.code, level_code: ll })
-            }
-          />
-        ))}
-      </div>
-    ));
+  const levels = ['Perustaso', 'Keskitaso', 'Ylin taso'];
+  const languageLabels = (
+    <div className={classes.LanguageLabels}>
+      <span className={classes.LanguageLabel} />
+      {levels.map(level => (
+        <span key={level} className={classes.LanguageLabel}>
+          {level}
+        </span>
+      ))}
+    </div>
+  );
 
-    return (
-      <div className={classes.LanguageCheckboxes}>
-        {languageLabels}
-        {checkboxGrid}
-      </div>
-    );
-  }
-}
+  const checkboxGrid = LANGUAGES.map(l => (
+    <div key={l.name} className={classes.CheckboxGrid}>
+      <div className={classes.Language}>{l.name}</div>
+      {l.levels.map(ll => (
+        <Checkbox
+          key={ll}
+          className={classes.Checkbox}
+          languageCode={l.code}
+          languageLevel={ll}
+          checked={isSelected(l.code, ll)}
+          onChange={(code, level) => toggleChecked(code, level)}
+        />
+      ))}
+    </div>
+  ));
 
-export default LanguageCheckboxes;
+  return (
+    <div className={classes.LanguageCheckboxes}>
+      {languageLabels}
+      {checkboxGrid}
+    </div>
+  );
+};
+
+languageCheckboxes.propTypes = {
+  languages: PropTypes.array.isRequired,
+  onChange: PropTypes.func.isRequired,
+};
+
+export default languageCheckboxes;
