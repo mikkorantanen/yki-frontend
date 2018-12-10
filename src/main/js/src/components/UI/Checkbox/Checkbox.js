c0d82a1c8db43a3bd80721780a676fdb0e5ec4f1
Matias Kantele <matias.kantele@siili.com>
2018-12-10 18:01:42 +0200

    add custom checkbox component for language selection

--- src/main/js/src/components/UI/Checkbox/Checkbox.js
+++ src/main/js/src/components/UI/Checkbox/Checkbox.js
@@ -1,14 +1,22 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import classes from './Checkbox.module.css';
 
-const checkbox = props => {
-  return (
-    <label className={classes.Container}>
-      <input type="checkbox" onClick={props.clicked} />
-      <span className={classes.Checkmark} />
-    </label>
-  );
+const checkbox = props => (
+  <label className={classes.Container}>
+    <input
+      type="checkbox"
+      onChange={() => props.onChange(props.languageCode, props.languageLevel)}
+      defaultChecked={props.checked || false}
+    />
+    <span className={classes.Checkmark} />
+  </label>
+);
+
+checkbox.propTypes = {
+  onChange: PropTypes.func.isRequired,
+  checked: PropTypes.bool,
 };
 
 export default checkbox;
