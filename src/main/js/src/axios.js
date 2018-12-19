0d91d8e971946f29af40e7489b7567080fd12eb9
Tom Bertell <tom.bertell@siili.com>
2018-12-19 06:58:47 +0200

    Redirect to login when response status is 401

--- src/main/js/src/axios.js
+++ src/main/js/src/axios.js
@@ -4,4 +4,18 @@ const instance = axios.create({
   baseURL: '',
 });
 
+instance.interceptors.response.use(
+  response => {
+    return response;
+  },
+  error => {
+    if (error.response.status === 401) {
+      if (window.location.href.includes('jarjestajarekisteri')) {
+        window.location.href = 'auth/cas';
+      }
+    }
+    return error;
+  },
+);
+
 export default instance;
