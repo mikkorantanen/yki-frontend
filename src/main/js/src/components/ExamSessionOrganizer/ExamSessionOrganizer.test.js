086436a3f6ee7c6fb4f3e4156c38b8e416c71a5c
Tom Bertell <tom.bertell@siili.com>
2018-12-21 09:03:07 +0200

    Added basic test cases

---
+++ src/main/js/src/components/ExamSessionOrganizer/ExamSessionOrganizer.test.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { configure, shallow, mount } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+import { examSessionOrganizer as ExamSessionOrganizer } from './ExamSessionOrganizer';
+
+configure({ adapter: new Adapter() });
+
+const organization = {
+  oid: '1.2.246.562.10.28646781494',
+  nimi: { fi: 'Amiedu' },
+  postiosoite: {
+    postinumeroUri: 'posti_00381',
+    osoiteTyyppi: 'posti',
+    yhteystietoOid: '1.2.246.562.5.72680991303',
+    postitoimipaikka: 'HELSINKI',
+    osoite: 'PL 151',
+  },
+  yhteystiedot: [],
+};
+
+const organizer = {
+  oid: '1.2.246.562.10.28646781493',
+  agreement_start_date: '2018-01-01',
+  agreement_end_date: '2019-01-01',
+  contact_name: 'Iida Ikola',
+  contact_email: 'iida.ikola@amiedu.fi',
+  contact_phone_number: '0101234546',
+  languages: [
+    {
+      language_code: 'fin',
+      level_code: 'PERUS',
+    },
+  ],
+  extra: null,
+};
+
+describe('<ExamSessionOrganizer />', () => {
+  it('should render agreement and contact details', () => {
+    const wrapper = shallow(
+      <ExamSessionOrganizer
+        t={key => key}
+        organizer={organizer}
+        organization={organization}
+      />,
+    );
+    expect(wrapper.find('.Languages').exists()).toBeTruthy();
+    expect(wrapper.find('.Contact').exists()).toBeTruthy();
+  });
+});
