3ca2b073d201f02ef81edcc3aa0a045fc8539029
Tom Bertell <tom.bertell@siili.com>
2018-12-21 09:03:17 +0200

    Added translations

--- src/main/js/src/components/ExamSessionOrganizer/ExamSessionOrganizer.js
+++ src/main/js/src/components/ExamSessionOrganizer/ExamSessionOrganizer.js
@@ -8,10 +8,10 @@ import { collectRegistryItemDetails } from '../../util/registryUtil';
 import { getLanguagesWithLevelDescriptions } from '../../util/registryUtil';
 import { DATE_FORMAT } from '../../common/Constants';
 
-const examSessionOrganizer = props => {
+export const examSessionOrganizer = props => {
   const item = collectRegistryItemDetails(
-    props.examSessionContent.organizer,
-    props.examSessionContent.organization,
+    props.organizer,
+    props.organization,
     props.lng,
   );
 
@@ -26,7 +26,7 @@ const examSessionOrganizer = props => {
 
   const contact = (
     <div className={classes.Contact}>
-      <h3>Yhteyshenkilön tiedot</h3>
+      <h3>{props.t('registryItem.contactHeader')}</h3>
       <p>{item.contact.name}</p>
       <p>{item.contact.email}</p>
       <p>{item.contact.phone}</p>
@@ -36,7 +36,7 @@ const examSessionOrganizer = props => {
 
   const agreement = (
     <div>
-      <h3>Voimassaoloaika</h3>
+      <h3>{props.t('common.validityPeriod')}</h3>
       <p data-cy="exam-session-organizer-agreement-validity">
         {moment(item.agreement.start).format(DATE_FORMAT)} -{' '}
         {moment(item.agreement.end).format(DATE_FORMAT)}
@@ -45,23 +45,24 @@ const examSessionOrganizer = props => {
   );
 
   return (
-    <div className={classes.ExamSessionOrganizer} data-cy="exam-session-organizer-agreement">
-      <h2>Voimassa olevat järjestäjäsopimukset</h2>
+    <div
+      className={classes.ExamSessionOrganizer}
+      data-cy="exam-session-organizer-agreement"
+    >
+      <h2>{props.t('common.agreement')}</h2>
       <div className={classes.AgreementGrid}>
         {languages}
         {agreement}
       </div>
-      <h2>Yhteystiedot</h2>
+      <h2>{props.t('registryItem.contact')}</h2>
       <div>{contact}</div>
-      <button className={classes.Update} onClick={props.clicked}>
-        Muuta yhteystietoja
-      </button>
     </div>
   );
 };
 
 examSessionOrganizer.propTypes = {
-  examSessionContent: PropTypes.object.isRequired,
+  organizer: PropTypes.object.isRequired,
+  organization: PropTypes.object.isRequired,
 };
 
 export default withNamespaces()(examSessionOrganizer);
--- src/main/js/src/components/UpcomingExamSessions/UpcomingExamSessions.js
+++ src/main/js/src/components/UpcomingExamSessions/UpcomingExamSessions.js
@@ -9,51 +9,40 @@ import {
   DATE_FORMAT_WITHOUT_YEAR,
   CODE_TO_LEVEL,
 } from '../../common/Constants';
-import Spinner from '../UI/Spinner/Spinner';
 import { languageToString } from '../../util/registryUtil';
 
-const upcomingExamSessions = props => {
-  const examSessionRows = props.loading ? (
-    <Spinner />
-  ) : (
-    props.examSessions.map((e, i) => {
-      const registrationOpen = moment().isSameOrAfter(
-        moment(e.registration_start_date),
-      );
-      return (
-        <div className={classes.Row} key={i} data-cy="exam-sessions-table-row">
-          <p>
-            {moment(e.session_date).format(DATE_FORMAT)}
-          </p>
-          <p>
-            {languageToString(e.language_code).toLowerCase()}
-          </p>
-          <p>{CODE_TO_LEVEL[e.level_code]}</p>
-          <p>
-            {moment(e.registration_start_date).format(DATE_FORMAT_WITHOUT_YEAR)}
-            &ndash;
-            {moment(e.registration_end_date).format(DATE_FORMAT)}
-          </p>
-          <p>
-            {registrationOpen
-              ? `${e.participants} / ${e.max_participants}`
-              : '-'}
-          </p>
-        </div>
-      );
-    })
-  );
+export const upcomingExamSessions = props => {
+  const examSessionRows = props.examSessions.map((e, i) => {
+    const registrationOpen = moment().isSameOrAfter(
+      moment(e.registration_start_date),
+    );
+    return (
+      <div className={classes.Row} key={i} data-cy="exam-sessions-table-row">
+        <p>{moment(e.session_date).format(DATE_FORMAT)}</p>
+        <p>{languageToString(e.language_code).toLowerCase()}</p>
+        <p>{CODE_TO_LEVEL[e.level_code]}</p>
+        <p>
+          {moment(e.registration_start_date).format(DATE_FORMAT_WITHOUT_YEAR)}
+          &ndash;
+          {moment(e.registration_end_date).format(DATE_FORMAT)}
+        </p>
+        <p>
+          {registrationOpen ? `${e.participants} / ${e.max_participants}` : '-'}
+        </p>
+      </div>
+    );
+  });
 
   return (
     <div className={classes.ExamSessionList}>
       <h2>{props.t('examSession.upcomingExamSessions')}</h2>
       {props.examSessions.length > 0 ? (
         <div className={classes.Grid} data-cy="exam-sessions-table">
-            <h3>{props.t('common.examDate')}</h3>
-            <h3>{props.t('common.language')}</h3>
-            <h3>{props.t('common.level')}</h3>
-            <h3>{props.t('common.registationPeriod')}</h3>
-            <h3>{props.t('examSession.participants')}</h3>
+          <h3>{props.t('common.examDate')}</h3>
+          <h3>{props.t('common.language')}</h3>
+          <h3>{props.t('common.level')}</h3>
+          <h3>{props.t('common.registationPeriod')}</h3>
+          <h3>{props.t('examSession.participants')}</h3>
           {examSessionRows}
         </div>
       ) : (
@@ -64,7 +53,6 @@ const upcomingExamSessions = props => {
 };
 
 upcomingExamSessions.propTypes = {
-  organizer: PropTypes.object.isRequired,
   examSessions: PropTypes.array.isRequired,
 };
 
