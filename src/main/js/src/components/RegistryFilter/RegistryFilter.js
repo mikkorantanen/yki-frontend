bb4a8cd356a675669ff0539bc36fd40427fa48df
Matias Kantele <matias.kantele@siili.com>
2018-12-27 15:30:03 +0200

    add select field for filtering registry by language

--- src/main/js/src/components/RegistryFilter/RegistryFilter.js
+++ src/main/js/src/components/RegistryFilter/RegistryFilter.js
@@ -3,6 +3,7 @@ import { withNamespaces } from 'react-i18next';
 
 import classes from './RegistryFilter.module.css';
 import { filterByNameOrLocation } from '../../util/registryUtil';
+import { LANGUAGES } from '../../common/Constants';
 
 class RegistryFilter extends Component {
   state = {
@@ -15,13 +16,20 @@ class RegistryFilter extends Component {
     this.setState({ input: event.target.value });
   };
 
+  languageSelectHandler = event => {
+    this.setState({ language: event.target.value });
+  };
+
   notFiltering = () =>
     this.state.input === '' &&
     this.state.language === '' &&
     this.state.level === '';
 
   componentDidUpdate = (prevProps, prevState) => {
-    if (prevState.input !== this.state.input) {
+    if (
+      prevState.input !== this.state.input ||
+      prevState.language !== this.state.language
+    ) {
       if (this.notFiltering()) {
         this.props.onChange(false, []);
       } else {
@@ -44,6 +52,28 @@ class RegistryFilter extends Component {
   };
 
   render() {
+    const languageSelect = (
+      <select
+        value={this.state.language}
+        className={[
+          classes.Language,
+          this.state.language === ''
+            ? classes.Placeholder
+            : classes.LanguageSelected,
+        ].join(' ')}
+        onChange={this.languageSelectHandler}
+      >
+        <option value="">
+          {this.props.t('registry.search.languageSelectPlaceholder')}
+        </option>
+        {LANGUAGES.map(l => (
+          <option key={l.code} value={l.code}>
+            {l.name}
+          </option>
+        ))}
+      </select>
+    );
+
     return (
       <div className={classes.Filter}>
         <input
@@ -51,6 +81,7 @@ class RegistryFilter extends Component {
           placeholder={this.props.t('registry.search.placeholder')}
           onChange={this.inputChangeHandler}
         />
+        {languageSelect}
       </div>
     );
   }
