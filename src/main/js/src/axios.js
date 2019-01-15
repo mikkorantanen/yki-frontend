89032e0ee622f209e5dc4c603c9a6bf1020903ad
Tom Bertell <tom.bertell@siili.com>
2019-01-15 10:33:03 +0200

    Add success-redirect to authentication redirect url

--- src/main/js/src/axios.js
+++ src/main/js/src/axios.js
@@ -14,7 +14,7 @@ instance.interceptors.response.use(
         window.location.href.includes('jarjestajarekisteri') ||
         window.location.href.includes('tutkintotilaisuudet')
       ) {
-        window.location.href = 'auth/cas';
+        window.location.href = `auth/cas?success-redirect=${window.location.href}`;
       }
     }
     return Promise.reject(error);
