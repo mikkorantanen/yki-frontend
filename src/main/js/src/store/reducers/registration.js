9760ded5f91fa59558b1df638a0b3ac68eff2a12
Matias Kantele <matias.kantele@siili.com>
2019-01-21 16:37:24 +0200

    add actions and reducers for choosing registration filters

--- src/main/js/src/store/reducers/registration.js
+++ src/main/js/src/store/reducers/registration.js
@@ -1,9 +1,28 @@
-// import * as actionTypes from '../actions/actionTypes';
+import * as actionTypes from '../actions/actionTypes';
 
-const initialState = {};
+const initialState = {
+  language: '',
+  level: '',
+  area: '',
+};
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
+    case actionTypes.SELECT_LANGUAGE:
+      return {
+        ...state,
+        language: action.language,
+      };
+    case actionTypes.SELECT_LEVEL:
+      return {
+        ...state,
+        level: action.level,
+      };
+    case actionTypes.SELECT_AREA:
+      return {
+        ...state,
+        area: action.area,
+      };
     default:
       return state;
   }
