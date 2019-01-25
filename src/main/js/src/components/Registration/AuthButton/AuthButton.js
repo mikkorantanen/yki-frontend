d7ac19368f982e7598cc8fea0c7941ee79d970ab
Tom Bertell <tom.bertell@siili.com>
2019-01-25 15:26:41 +0200

    Suomi.fi authentication button

---
+++ src/main/js/src/components/Registration/AuthButton/AuthButton.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { withNamespaces } from 'react-i18next';
+import PropTypes from 'prop-types';
+
+import Button from '../../UI/Button/Button';
+
+const authButton = props => (
+  <form action={'/yki/auth/'}>
+    <input type="hidden" name="examSessionId" value={props.examSessionId} />{' '}
+    <Button type="submit" isParticipant={true}>
+      {props.t('registration.auth.button')}
+    </Button>
+  </form>
+);
+
+authButton.propTypes = {
+  examSessionId: PropTypes.number.isRequired,
+};
+
+export default withNamespaces()(authButton);
