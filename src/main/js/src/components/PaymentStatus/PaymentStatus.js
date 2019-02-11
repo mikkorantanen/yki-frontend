b665eb1e33c72c7bba8564797ddda8b30908d8a6
Tom Bertell <tom.bertell@siili.com>
2019-02-11 07:04:26 +0200

    Changes to payment success page

--- src/main/js/src/components/PaymentStatus/PaymentStatus.js
+++ src/main/js/src/components/PaymentStatus/PaymentStatus.js
@@ -6,8 +6,34 @@ import queryString from 'query-string';
 import Alert from '../Alert/Alert';
 import Page from '../../hoc/Page/Page';
 
+import classes from './PaymentStatus.module.css';
+
+
 const status = props => {
   const queryParams = queryString.parse(props.location.search);
+
+  const paymentSuccess = (
+    <div className={classes.PaymentStatus}>
+      <div>
+        <h1 data-cy="registration-success-header">
+          {props.t('payment.status.success')}
+        </h1>
+        <p>{props.t('registration.success.info1')}:</p>
+      </div>
+      <div>
+        <p>
+          {props.t('registration.success.info2')}
+        </p>
+      </div>
+      <div className={classes.InfoBox}>
+        <p>
+          <b>{props.t('registration.success.info3')}</b>{' '}
+          {props.t('registration.success.info4')}
+        </p>
+      </div>
+    </div>
+  );
+
   switch (queryParams.status) {
     case 'payment-success': {
       return <Alert title={props.t('payment.status.success')} success={true} />;
