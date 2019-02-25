0ccfbd9cd9cdd773a268195fed05fec38637da63
Tom Bertell <tom.bertell@siili.com>
2019-02-25 10:22:52 +0200

    Link expired page

---
+++ src/main/js/src/components/LinkExpired/LinkExpired.test.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import toJson from 'enzyme-to-json';
+
+import { linkExpired as LinkExpired } from './LinkExpired';
+
+configure({ adapter: new Adapter() });
+
+describe('<LinkExpired />', () => {
+  it('should render link expired page', () => {
+    const wrapper = shallow(
+      <LinkExpired
+        t={key => key}
+        match={{ path: '/ilmoittautuminen/vanhentunut' }}
+      />,
+    );
+    expect(toJson(wrapper)).toMatchSnapshot();
+  });
+});
