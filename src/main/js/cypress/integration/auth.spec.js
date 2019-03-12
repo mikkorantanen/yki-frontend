5c933a6032211ee3ce9c667f4c1fe3b405723d69
Tom Bertell <tom.bertell@siili.com>
2019-03-12 12:26:06 +0200

    Execute authorization tests against untuva environment

--- src/main/js/cypress/integration/auth.spec.js
+++ src/main/js/cypress/integration/auth.spec.js
@@ -1,12 +1,18 @@
 describe('Authorization', () => {
-  if (Cypress.config('baseUrl').includes('https')) {
-    it('init session endpoint should not be accessible', () => {
-      cy.request({
-        url: '/yki/auth/initsession',
-        failOnStatusCode: false,
-      }).then(response => {
-        expect(response.status).to.eq(404);
-      });
+  it('init session endpoint should not be accessible in yki domain', () => {
+    cy.request({
+      url: 'https://yki.untuvaopintopolku.fi/yki/auth/initsession',
+      failOnStatusCode: false,
+    }).then(response => {
+      expect(response.status).to.eq(404);
     });
-  }
+  });
+  it('init session endpoint should not be accessible in virkailija domain', () => {
+    cy.request({
+      url: 'https://virkailija.untuvaopintopolku.fi/yki/auth/initsession',
+      failOnStatusCode: false,
+    }).then(response => {
+      expect(response.status).to.eq(404);
+    });
+  });
 });
