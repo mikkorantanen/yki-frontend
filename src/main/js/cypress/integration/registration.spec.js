59eb8d78cca8fe5583e60885bd5a9b7edced3a6b
Matias Kantele <matias.kantele@siili.com>
2019-03-18 12:18:29 +0200

    add cypress test for notification signup

--- src/main/js/cypress/integration/registration.spec.js
+++ src/main/js/cypress/integration/registration.spec.js
@@ -39,9 +39,13 @@ describe('Registration', () => {
     cy.get('select').contains('Suomi');
     cy.get('select').contains('Perustaso');
 
-    cy.log('Shows location only once regardless of case')
-    cy.get('select').contains('Jyväskylä').should('exist');
-    cy.get('select').contains('JYVÄSKYLÄ').should('not.exist');
+    cy.log('Shows location only once regardless of case');
+    cy.get('select')
+      .contains('Jyväskylä')
+      .should('exist');
+    cy.get('select')
+      .contains('JYVÄSKYLÄ')
+      .should('not.exist');
 
     cy.get('select').contains('Koko maa');
   });
@@ -80,6 +84,14 @@ describe('Registration', () => {
     cy.visit('/ilmoittautuminen/valitse-tutkintotilaisuus');
     cy.contains('Ilmoittaudu varasijalle').click();
     cy.contains('Olet ilmoittautumassa varasijalle tutkintotilaisuuteen');
+  });
+
+  it('Notification signup works', () => {
+    cy.visit('/ilmoittautuminen/valitse-tutkintotilaisuus');
+    cy.contains('Ilmoittaudu varasijalle').click();
+    cy.get('input#email').type('test@test.com');
+    cy.contains('Lähetä').click();
+    cy.contains('test@test.com');
   });
 
   it('Login link expired page exists', () => {
