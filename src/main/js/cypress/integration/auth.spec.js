e44a850e5de1183048ed156755c27be096494577
Tom Bertell <tom.bertell@siili.com>
2019-04-02 10:22:10 +0300

    Added smoke tests to be executed on CI

--- src/main/js/cypress/integration/auth.spec.js
+++ src/main/js/cypress/integration/auth.spec.js
@@ -1,18 +1,22 @@
 describe('Authorization', () => {
-  it('init session endpoint should not be accessible in yki domain', () => {
-    cy.request({
-      url: 'https://yki.untuvaopintopolku.fi/yki/auth/initsession',
-      failOnStatusCode: false,
-    }).then(response => {
-      expect(response.status).to.eq(404);
+  if (Cypress.env('ADMIN_USER_PASSWORD').length > 0) {
+    it('init session endpoint should not be accessible in yki domain', () => {
+      cy.request({
+        url: 'https://yki.untuvaopintopolku.fi/yki/auth/initsession',
+        failOnStatusCode: false,
+      }).then(response => {
+        expect(response.status).to.eq(404);
+      });
     });
-  });
-  it('init session endpoint should not be accessible in virkailija domain', () => {
-    cy.request({
-      url: 'https://virkailija.untuvaopintopolku.fi/yki/auth/initsession',
-      failOnStatusCode: false,
-    }).then(response => {
-      expect(response.status).to.eq(404);
+    it('init session endpoint should not be accessible in virkailija domain', () => {
+      cy.request({
+        url: 'https://virkailija.untuvaopintopolku.fi/yki/auth/initsession',
+        failOnStatusCode: false,
+      }).then(response => {
+        expect(response.status).to.eq(404);
+      });
     });
-  });
+  } else {
+    it('authorization tests are executed only on CI', () => {});
+  }
 });
---
+++ src/main/js/cypress/integration/smoke.spec.js
@@ -0,0 +1,35 @@
+describe('Smoke test', () => {
+  if (Cypress.env('ADMIN_USER_PASSWORD').length > 0) {
+    it('registration front page loads', () => {
+      cy.visit('https://yki.untuvaopintopolku.fi');
+      cy.get('[data-cy=continue-button]').should('exist');
+    });
+    it('organizer registry front page loads', () => {
+      cy.visit('https://virkailija.untuvaopintopolku.fi/yki/auth/cas');
+      cy.get('[id=username]').type('ykitestaaja');
+      cy.get('[id=password]').type(Cypress.env('ADMIN_USER_PASSWORD'));
+      cy.get('input')
+        .contains('Kirjaudu')
+        .click();
+
+      cy.get('h1')
+        .contains('Kielitutkintojen järjestäjärekisteri')
+        .should('exist');
+      cy.get('[data-cy=registry-item]').should('exist');
+    });
+    it('exam sessions front page loads', () => {
+      cy.visit('https://virkailija.untuvaopintopolku.fi/yki/auth/cas');
+      cy.get('[id=username]').type('ykijarjestaja');
+      cy.get('[id=password]').type(Cypress.env('ORG_USER_PASSWORD'));
+      cy.get('input')
+        .contains('Kirjaudu')
+        .click();
+
+      cy.get('h2')
+        .contains('Tulevat tutkintotilaisuudet')
+        .should('exist');
+    });
+  } else {
+    it('smoke tests are executed only on CI', () => {});
+  }
+});
