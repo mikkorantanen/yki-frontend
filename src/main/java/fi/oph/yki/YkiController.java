109f58beb9250b1436478f8611c7e1843cbb9536
Tom Bertell <tom.bertell@siili.com>
2019-04-05 08:27:01 +0300

    Redirect root uri to /auth/cas

--- src/main/java/fi/oph/yki/YkiController.java
+++ src/main/java/fi/oph/yki/YkiController.java
@@ -8,6 +8,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class YkiController {
 
+    @GetMapping(value = "/")
+    public String rootRedirect() {
+        return "redirect:/auth/cas";
+    }
+
     @GetMapping(value = {
             "/ilmoittautuminen",
             "/tutkintopaivat",
