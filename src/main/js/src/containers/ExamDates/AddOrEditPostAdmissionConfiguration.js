6d3da763c3e78c7d317bb42294289e8dc8662345
Hasse Sarin <hasse.sarin@siili.com>
2020-02-03 09:18:30 +0200

    Split views to components

--- src/main/js/src/containers/ExamDates/AddOrEditPostAdmissionConfiguration.js
+++ src/main/js/src/containers/ExamDates/AddOrEditPostAdmissionConfiguration.js
@@ -2,36 +2,47 @@ import React, { Component } from 'react';
 import moment from 'moment';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
-import DatePicker from '../../../../components/UI/DatePicker/DatePicker';
+import DatePicker from '../../components/UI/DatePicker/DatePicker';
 import { DATE_FORMAT } from '../../common/Constants';
 import { languageToString } from '../../util/util';
 import classes from './AddOrEditPostAdmissionConfiguration.module.css';
 
 // olemassaolevaa konfiguraatiota ei voi poistaa jos jälki-ilmoperiodi on käynnissä
 
-const AddOrEditPostAdmissionConfiguration = ({ examDate }) => {
-  return (examDate.post_admission_end_date ?
-    (
-      <div>
-        <h1>Muokkaa jälki-ilmoittautumisasetuksia</h1>
-        {/* { tähän jotain helpparitekstejä kertomaan jos konfiguraatiota ei voi poistaa } */}
-
-        <ExamDateDetails examDate={examDate.exam_date} languages={examDate.languages} />
-      </div>
-    ) :
-    (
-      <div>
-        <h1>Luo jälki-ilmoittautumisasetukset</h1>
-        <ExamDateDetails examDate={examDate.exam_date} languages={examDate.languages} />
-      </div>
-    ))
-}
-
 const ExamDateDetails = ({ examDate, languages }) => (
-  <div>
+  <>
     <p>Tutkintopäivä: {moment(examDate).format(DATE_FORMAT)}</p>
     <p>Kielet: {languages.map(l => languageToString(l.language_code).toLowerCase()).join(', ')}</p>
-  </div>
+  </>
 );
+
+const FormFields = examDates => (
+  <Formik>
+
+  </Formik>
+)
+
+const editView = examDate => (
+  <>
+    <h1>Muokkaa jälki-ilmoittautumisasetuksia</h1>
+    {/* { tähän jotain helpparitekstejä kertomaan jos konfiguraatiota ei voi poistaa } */}
+
+    <ExamDateDetails examDate={examDate.exam_date} languages={examDate.languages} />
+  </>
+);
+
+const createView = examDate => (
+  <>
+    <h1>Luo jälki-ilmoittautumisasetukset</h1>
+    <ExamDateDetails examDate={examDate.exam_date} languages={examDate.languages} />
+  </>
+);
+
+const AddOrEditPostAdmissionConfiguration = ({ examDate }) => {
+  console.log(examDate)
+  return (
+    examDate.post_admission_end_date ? editView(examDate) : createView(examDate)
+  )
+}
 
 export default AddOrEditPostAdmissionConfiguration;
