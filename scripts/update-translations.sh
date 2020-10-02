438a36ea7413617f49c96a4f1d854d10b939507e
mikkorantanen <mikko.rantanen@siili.com>
2020-10-02 09:34:00 +0300

    new localizations and script update

--- scripts/update-translations.sh
+++ scripts/update-translations.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-curl -X GET https://virkailija.untuvaopintopolku.fi/yki/api/localisation?lang=fi | node <<< "var o = $(cat); var ord = {}; Object.keys(o).sort().forEach(function(key) {ord[key] = o[key];}); console.log(JSON.stringify(ord, null, 2));"  > ../src/main/js/dev/rest/localisation/translations_fi.json
-curl -X GET https://virkailija.untuvaopintopolku.fi/yki/api/localisation?lang=sv | node <<< "var o = $(cat); var ord = {}; Object.keys(o).sort().forEach(function(key) {ord[key] = o[key];}); console.log(JSON.stringify(ord, null, 2));"  > ../src/main/js/dev/rest/localisation/translations_sv.json
-curl -X GET https://virkailija.untuvaopintopolku.fi/yki/api/localisation?lang=en | node <<< "var o = $(cat); var ord = {}; Object.keys(o).sort().forEach(function(key) {ord[key] = o[key];}); console.log(JSON.stringify(ord, null, 2));"  > ../src/main/js/dev/rest/localisation/translations_en.json
+curl -X GET https://virkailija.opintopolku.fi/yki/api/localisation?lang=fi | node <<< "var o = $(cat); var ord = {}; Object.keys(o).sort().forEach(function(key) {ord[key] = o[key];}); console.log(JSON.stringify(ord, null, 2));"  > ../src/main/js/dev/rest/localisation/translations_fi.json
+curl -X GET https://virkailija.opintopolku.fi/yki/api/localisation?lang=sv | node <<< "var o = $(cat); var ord = {}; Object.keys(o).sort().forEach(function(key) {ord[key] = o[key];}); console.log(JSON.stringify(ord, null, 2));"  > ../src/main/js/dev/rest/localisation/translations_sv.json
+curl -X GET https://virkailija.opintopolku.fi/yki/api/localisation?lang=en | node <<< "var o = $(cat); var ord = {}; Object.keys(o).sort().forEach(function(key) {ord[key] = o[key];}); console.log(JSON.stringify(ord, null, 2));"  > ../src/main/js/dev/rest/localisation/translations_en.json
 
 
